0f957022a8578a10206abddf92d0af39cc7f9aff
NithilanRengapragash <116626@lrstudents.org>
2023-01-17 17:18:42 -0500

    Pivot manipulator blind coded?!!!
    Pivot arm blind coded

--- RedWatch2023/src/main/java/frc/robot/commands/armJoint.java
+++ RedWatch2023/src/main/java/frc/robot/commands/armJoint.java
@@ -18,12 +18,26 @@ public class armJoint extends CommandBase{
     @Override 
     public void initialize(){
         // define the encoder thing ??!!
-        m_pinkArm.encoderReset(m_pinkArm.m_pinkArmEncoder);        
+        m_pinkArm.encoderReset(m_pinkArm.m_pivotEncoder);  
+
     }
 
     @Override
     public void execute(){
-        m_pinkArm.turnMotor(m_pinkArm.m_pinkArmEncoder);
+        m_pinkArm.turnMotor(m_pinkArm.m_pivot, false);
+    }
+    @Override 
+    public void end(boolean interrupted){
+        m_pinkArm.encoderReset(m_pinkArm.m_pivotEncoder);
     }
 
+        // ADD A SOFTWARE LIMIT FOR THE ARM
+/**     @Override
+    public boolean isFinished() {
+      if (m_pivot.getRightDistance() > kClimberRightSize) {
+        return true;
+      } else {
+        return false;
+      }
+    }*/
 }
--- RedWatch2023/src/main/java/frc/robot/subsystems/pinkArm.java
+++ RedWatch2023/src/main/java/frc/robot/subsystems/pinkArm.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import static frc.robot.Constants.pinkArmConstants.*;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
@@ -15,13 +17,87 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.SPI;
 import com.kauailabs.navx.frc.AHRS;
 
+
+
+
+
 public class pinkArm extends SubsystemBase {
+
+  private static AHRS m_ahrs;
+
+  public final CANSparkMax m_pivot;
+
+  public final RelativeEncoder m_pivotEncoder;
   /** Creates a new ExampleSubsystem. */
   public pinkArm() {
+      m_pivot = new CANSparkMax(kJoint1Port, MotorType.kBrushless);
+      
+      setMotor(m_pivot, false);
+      m_pivotEncoder = m_pivot.getEncoder();
+      positionEncoderInit(m_pivotEncoder);
+      
+      try {
+        m_ahrs = new AHRS(SPI.Port.kMXP);
+      } 
+      catch (RuntimeException ex){
+        DriverStation.reportError("Error instantiating navX MXP: " + ex.getMessage(), true);
+      }
+    }
+  
+    public void changeMode(String mode) {
+  
+    }
+  
+    public void turnMotor(CANSparkMax motor, boolean inverse) {
+      if (inverse) {
+        motor.set(-0.1);
+      }
+      else {
+        motor.set(0.1);
+      }
+    }
+  
+  
+    private void pivotEncoderInit(RelativeEncoder encoder) {
+      encoder.setPositionConversionFactor(kAnglePerRevolution);
+    }
+  
+    public void encoderReset(RelativeEncoder encoder) {
+      encoder.setPosition(0);
+    }
+  
+    public double getLeftDistance() {
+      return -m_pivotEncoder.getPosition();
+    }
+
+    private void positionEncoderInit(RelativeEncoder encoder) {
+      encoder.setPositionConversionFactor(kDistancePerRevolution);
+  
+      encoderReset(encoder);
+    }
+
+    public void setMotor(CANSparkMax motor, boolean inverse) {
+      motor.restoreFactoryDefaults();
+      motor.setIdleMode(IdleMode.kBrake);
+      motor.setInverted(inverse);
+    }
+  
+    public double getGyroAngle(){
+      return m_ahrs.getAngle();
+    }
+  
+    public void resetGyroAngle(){
+      m_ahrs.reset();
+    }
+  
 
 
 
-  }
+
+
+
+
+
 
   /**
    * Example command factory method.
