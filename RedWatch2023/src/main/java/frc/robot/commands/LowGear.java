ecc6dc8fbd9302c0451bd2aba6b361ceb56bc28d
Krithik Alluri <Krithik.alluri@gmail.com>
2023-02-06 15:30:31 -0500

    Revert "Merge pull request #4 from 2729StormRobotics/TelescopingArm"
    This reverts commit 5c094d54f9afba689b89775498dc4ef109dceb91, reversing
    changes made to 01b81d533b5b060e618b85a3c6c2a1645388c9b5.

--- RedWatch2023/src/main/java/frc/robot/commands/LowGear.java
+++ RedWatch2023/src/main/java/frc/robot/commands/LowGear.java
@@ -1,26 +1,28 @@
+/**
+ * divides the power of the drivetrain motors by
+   a lower value to speed up the robot
+ */
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.TelescopingArmCommands;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.MeasuringPotentiometer;
+import frc.robot.Constants;
+import frc.robot.subsystems.Drivetrain;
 
-public class ResetPot extends CommandBase {
-  /** Creates a new ResetPot. */
-  private MeasuringPotentiometer m_pot;
-  public ResetPot(MeasuringPotentiometer pot) {
-    m_pot = pot;
+public class LowGear extends CommandBase {
+  /** Creates a new ChangeGear. */
+  public LowGear() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(pot);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
-    m_pot.offset = m_pot.pot.get()*50;
+    Drivetrain.speedLimiter = 3.5;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -34,8 +36,6 @@ public class ResetPot extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    
-    m_pot.offset = m_pot.pot.get()*50;
     return true;
   }
 }
---
+++ RedWatch2023/src/main/java/frc/robot/commands/pivotArm/armJoint.java
@@ -0,0 +1,61 @@
+package frc.robot.commands.pivotArm;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.BooleanSupplier; 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.PivotArm;
+import frc.robot.Constants.pinkArmConstants.*;
+
+public class armJoint extends CommandBase{
+    private final PivotArm m_pinkArm;
+    private final DoubleSupplier m_ArmSpeed;
+    private final BooleanSupplier m_leftBumper;
+    private final BooleanSupplier m_rightBumper;
+
+
+ 
+/** Allows the joint on the pink arm to turn
+*  @param ArmSpeed
+*  @param ArmBumper
+*  @param subsystem
+* */ 
+
+    public armJoint(DoubleSupplier armSpeed, BooleanSupplier leftBumper, BooleanSupplier rightBumper, PivotArm subsystem) {
+        m_pinkArm = subsystem;
+        m_ArmSpeed = armSpeed;
+        m_leftBumper = leftBumper;
+        m_rightBumper = rightBumper;
+        addRequirements(m_pinkArm);
+    }
+
+    @Override 
+    public void initialize(){
+        //Stops the motor when intialized
+        m_pinkArm.m_pivot.stopMotor();
+        //Sets the pivot encoder position to zero when initialized
+        m_pinkArm.encoderReset(m_pinkArm.m_pivotEncoder);  
+
+    }
+
+    @Override
+    public void execute(){
+        if(m_rightBumper.getAsBoolean() && m_pinkArm.m_pivotEncoder.getPosition() <= 1.0){
+            m_pinkArm.turnMotor(m_pinkArm.m_pivot, false);
+        }
+        else if (m_leftBumper.getAsBoolean() && m_pinkArm.m_pivotEncoder.getPosition() >= 0.03) {
+            m_pinkArm.turnMotor(m_pinkArm.m_pivot, true);
+        }
+        else {
+            m_pinkArm.m_pivot.set(0);
+        }
+    }
+    @Override 
+    public void end(boolean interrupted){
+        m_pinkArm.m_pivot.stopMotor();
+    }
+
+     @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
