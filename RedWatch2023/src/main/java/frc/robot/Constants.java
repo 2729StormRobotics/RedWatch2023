92081a13e4bbe394a53638d522426ecd4ff4a21c
AkshayM1 <122124@lrstudents.org>
2023-02-11 09:14:53 -0500

    Updated drivetrain motor port IDs

--- RedWatch2023/src/main/java/frc/robot/Constants.java
+++ RedWatch2023/src/main/java/frc/robot/Constants.java
@@ -2,6 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+ * Robo Rio/PDP ports can be referenced at 
+ * https://docs.google.com/spreadsheets/d/1bbRh-F-XOhQwSzRBP7F1Jo3ygR4dRqCi2I8FiAghLpA/edit#gid=0
+ */
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -56,10 +61,10 @@ public final class Constants {
   }
 
   public static final class DrivetrainConstants {
-    public static final int RIGHT_MOTOR2_ID = 36;
-    public static final int RIGHT_MOTOR_ID = 12;
-    public static final int LEFT_MOTOR2_ID = 3;
-    public static final int LEFT_MOTOR_ID = 14;
+    public static final int RIGHT_MOTOR2_ID = 2;
+    public static final int RIGHT_MOTOR_ID = 9;
+    public static final int LEFT_MOTOR2_ID = 17;
+    public static final int LEFT_MOTOR_ID = 18;
     public static final int STALL_LIMIT = 45;
     public static final int kDriverControllerPort = 1;
     public static final int kWeaponsControllerPort = 2;
