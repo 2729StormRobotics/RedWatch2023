3654b116e74128493df29bb24020264c871979ff
AkshayM1 <122124@lrstudents.org>
2023-02-11 10:56:43 -0500

    Fixed errors

--- RedWatch2023/src/main/java/frc/robot/commandgroups/AutoScore.java
+++ RedWatch2023/src/main/java/frc/robot/commandgroups/AutoScore.java
@@ -2,10 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commandgroups;
+package frc.robot.CommandGroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commandgroups.AutoScoreCmds.AutoScorePivotHighCone;
+import frc.robot.CommandGroups.AutoScoreCmds.AutoScorePivotHighCone;
 import frc.robot.commands.Gripper.EjectItem;
 import frc.robot.subsystems.Gripper;
 import frc.robot.subsystems.PivotArm;
