8c2c60c1a3c521d7f137f38002d5e93f7a6fdeea
AkshayM1 <122124@lrstudents.org>
2023-02-15 17:53:26 -0500

    test values for pivot

--- RedWatch2023/src/main/java/frc/robot/Constants.java
+++ RedWatch2023/src/main/java/frc/robot/Constants.java
@@ -155,8 +155,8 @@ public final class Constants {
     public static final int kGripperRightMotor = 9;
     public static final int kGripperLeftMotor = 5;
     // Variable assigned values can change depending on what is needed for the robot.
-    public static final double kGripperIntakeMotorSpeed = 0.20;   
-    public static final double kGripperEjectMotorSpeed = -0.2;
+    public static final double kGripperIntakeMotorSpeed = 0.35;   
+    public static final double kGripperEjectMotorSpeed = -0.35;
     public static final int kBeambreak = 1;
     }
 
--- RedWatch2023/src/main/java/frc/robot/commands/pivotArm/armJoint.java
+++ RedWatch2023/src/main/java/frc/robot/commands/pivotArm/armJoint.java
@@ -24,7 +24,7 @@ public class armJoint extends CommandBase{
     @Override 
     public void initialize(){
         //Stops the motor when intialized
-        m_pinkArm.m_pivot.stopMotor();
+         m_pinkArm.m_pivot.stopMotor();
          m_pinkArm.m_pivot2.stopMotor();
         //Sets the pivot encoder position to zero when initialized
         m_pinkArm.encoderReset(m_pinkArm.m_pivotEncoder);  
@@ -34,25 +34,25 @@ public class armJoint extends CommandBase{
     @Override
     public void execute(){
         if ((m_rightStick.getAsDouble() <= -0.85) &&( m_pinkArm.m_pivotEncoder.getPosition() <= 75)){
-            m_pinkArm.turnMotor(m_pinkArm.m_pivot, false);
-             m_pinkArm.turnMotor(m_pinkArm.m_pivot2, false);
+             m_pinkArm.turnMotor(m_pinkArm.m_pivot, false);
+             m_pinkArm.turnMotor(m_pinkArm.m_pivot2, true);
 
         }
         else if ((m_rightStick.getAsDouble() >= 0.85) &&( m_pinkArm.m_pivotEncoder.getPosition() <= 40)) {
-          m_pinkArm.turnMotor(m_pinkArm.m_pivot, false);
+           m_pinkArm.turnMotor(m_pinkArm.m_pivot, true);
            m_pinkArm.turnMotor(m_pinkArm.m_pivot2, false);
 
         }
         else {
-            m_pinkArm.m_pivot.set(0);
+             m_pinkArm.m_pivot.set(0);
              m_pinkArm.m_pivot2.set(0);
 
         }
     }
     @Override 
     public void end(boolean interrupted){
-        m_pinkArm.m_pivot.stopMotor();
-         m_pinkArm.m_pivot2.stopMotor();
+          m_pinkArm.m_pivot.stopMotor();
+          m_pinkArm.m_pivot2.stopMotor();
 
     }
 
