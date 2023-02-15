a350694abb47f7cdb2cdc2dd59e64726946384c0
Krithik Alluri <Krithik.alluri@gmail.com>
2023-02-15 17:24:24 -0500

    Fix the CommandGroups error

--- RedWatch2023/src/main/java/frc/robot/commandgroups/AutoScoreCmds/AutoScorePivotHighCone.java
+++ RedWatch2023/src/main/java/frc/robot/commandgroups/AutoScoreCmds/AutoScorePivotHighCone.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commandgroups.AutoScoreCmds;
+package frc.robot.CommandGroups.AutoScoreCmds;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Gripper;
 import frc.robot.subsystems.Lights;
