35afeac09b41f54094abb6a1c8a6c83cf52b88f1
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2023-04-05 15:26:56 -0400

    meltdown

--- RedWatch2023/src/main/java/frc/robot/commands/Meltdown.java
+++ RedWatch2023/src/main/java/frc/robot/commands/Meltdown.java
@@ -12,15 +12,27 @@ import frc.robot.subsystems.TelescopingArm;
 
 public class Meltdown extends CommandBase {
   /** Creates a new Meltdown. */
-
+  private final Drivetrain m_drivetrain;
+  private final Gripper m_gripper;
+  private final PivotArm m_PivotArm;
+  private final TelescopingArm m_TelescopingArm;
   public Meltdown(Drivetrain drivetrain, Gripper gripper, PivotArm pivotarm, TelescopingArm telescopingarm) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrain = drivetrain;
+    m_gripper = gripper;
+    m_PivotArm = pivotarm;
+    m_TelescopingArm = telescopingarm;
   addRequirements(drivetrain, gripper, pivotarm, telescopingarm);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_drivetrain.stopDrive();
+    m_gripper.stopGripper();
+    m_PivotArm.turnMotor(0);
+    m_TelescopingArm.turnMotor(m_TelescopingArm.m_ArmExtend, 0);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -31,11 +43,17 @@ public class Meltdown extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_drivetrain.stopDrive();
+    m_gripper.stopGripper();
+    m_PivotArm.turnMotor(0);
+    m_TelescopingArm.turnMotor(m_TelescopingArm.m_ArmExtend, 0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    
     return true;
   }
 }
